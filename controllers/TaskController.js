05600f22b9d54bd9f01b9b982576f6cf0391db4f
Matheus Coutinho <matheuscoutinho.0420@gmail.com>
2025-05-05 17:55:26 -0300

    delete + edit

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -1,3 +1,4 @@
+const { where } = require('sequelize')
 const Task = require('../models/task.model')
 
 module.exports = class TaskController {
@@ -5,7 +6,7 @@ module.exports = class TaskController {
         res.render('tasks/create')
     }
 
-    static async createTaskSave(req, res){
+    static async createTaskSave(req, res) {
         const task = {
             title: req.body.title,
             description: req.body.description,
@@ -17,10 +18,39 @@ module.exports = class TaskController {
         res.redirect('/tasks')
     }
 
+    static async removeTask(req, res) {
+
+        const id = req.body.id
+        await Task.destroy({ where: { id: id } })
+
+        res.redirect('/tasks')
+    }
+
+    static async updateTask(req, res) {
+        const id = req.params.id
+
+        const task = await Task.findOne({ where: { id: id }, raw: true })
+
+        res.render('tasks/edit', { task })
+    }
+
+    static async updateTaskPost(req, res) {
+
+        const id = req.body.id
+        const task = {
+            title: req.body.title,
+            description: req.body.description
+        }
+
+        await Task.update(task, { where: { id: id }})
+
+        res.redirect('/tasks')
+    }
+
     static async showTasks(req, res) {
 
-        const tasks = await Task.findAll({raw: true})
+        const tasks = await Task.findAll({ raw: true })
 
-        res.render('tasks/all', {tasks})
+        res.render('tasks/all', { tasks })
     }
 }
